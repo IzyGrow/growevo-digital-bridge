345f3b9e001c992f71c62c2d86face662c14a8e2
IzyGrow <izygroww@gmail.com>
2025-07-01 07:24:32 +0300

    Update SuccessStories.tsx

--- src/components/SuccessStories.tsx
+++ src/components/SuccessStories.tsx
@@ -12,7 +12,7 @@ export const SuccessStories = () => {
       name: "Mehmet",
       location: "Antakya",
       story: "Zeytinyağı satışım dijital destek sayesinde 5 kata çıktı. Artık büyük şehirlere bile gönderim yapıyorum.",
-      image: "https://images.unsplash.com/photo-1571087292801-a9d5bae21368?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+      image: "https://unsplash.com/photos/olives-growing-on-an-olive-tree-in-the-sun-og9y6wZJE30"
     },
     {
       name: "Ayşe",
